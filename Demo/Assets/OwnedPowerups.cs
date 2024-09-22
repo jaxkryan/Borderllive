8c100821e8d598002835652b8cd5d36ff06ce473
Le Dang Huy <huyg.a22052003@gmail.com>
2024-09-22 12:44:07 +0700

    Update OwnedPowerups.cs

--- Demo/Assets/OwnedPowerups.cs
+++ Demo/Assets/OwnedPowerups.cs
@@ -17,20 +17,10 @@ public class OwnedPowerups : MonoBehaviour
         ActivatePowerup();
     }
 
-    //public void AddPowerup(Powerups powerup)
-    //{
-    //    if (!activePowerups.Contains(powerup))
-    //    {
-    //        activePowerups.Add(powerup);
-    //        ActivatePowerup(powerup); // Automatically activate when added
-    //    }
-    //}
-
     public void ActivatePowerup()
     {
         Debug.Log("so pu: " + activePowerups.Count);
         foreach (Powerups p in activePowerups) {
-            // Apply Metal_1 effect to the player
             if (p is Metal_1 metalPowerup)
             {
                 metalPowerup.ApplyEffect(playerController);
