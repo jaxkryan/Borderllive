a14e0e9cd7f1d7d3ad776fdbbbdd6b0bbba912fb
Luutu011 <luututkls2@gmail.com>
2024-10-09 14:01:01 +0700

    Revert "Merge branch 'Tu' into main"
    This reverts commit 5ef32b0d55edae215f2d2e6ce543a731125af949, reversing
    changes made to ce1d7fd64ec413c5060de5261f0fa0a8a3762c3d.

--- Demo/Assets/Objects/Item/Item.cs
+++ Demo/Assets/Objects/Item/Item.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Localization;
 using UnityEngine.UI;
 using static Item;
 
@@ -11,7 +10,6 @@ public abstract class Item : ScriptableObject
     // Item properties
     public string itemName;
     public string itemDescription;
-    public string historyDescription;
     public Sprite image;
     public enum ItemType { Active, Passive }
     public ItemType itemType;
@@ -25,23 +23,6 @@ public abstract class Item : ScriptableObject
     {
         return image != null ? image.name : null; // Get the sprite name
     }
-
-    public LocalizedString nameLocalization;
-    public LocalizedString descriptionLocalization;
-    public LocalizedString historyDescriptionLocalization; // Localization for historyDescription
-
-    public void InitializeLocalization(string nameKey, string descriptionKey, string historyKey)
-    {
-        nameLocalization = new LocalizedString { TableReference = "Items", TableEntryReference = nameKey };
-        descriptionLocalization = new LocalizedString { TableReference = "Items", TableEntryReference = descriptionKey };
-        historyDescriptionLocalization = new LocalizedString { TableReference = "Items", TableEntryReference = historyKey }; // Initialize localization for history description
-    }
-
-    public void UpdateHistoryLocalization(string historyKey)
-    {
-        historyDescriptionLocalization.TableEntryReference = historyKey;  // Method to update the history description localization
-    }
-
     [field: SerializeField]
     public List<ItemParameter> DefaultParametersList { get; set; }
     // // Virtual method to implement item pickup behavior
--- Demo/Assets/Objects/Item/Item1.cs
+++ Demo/Assets/Objects/Item/Item1.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -18,8 +18,6 @@ public class Item1 : Item
         this.cost = 100;
         this.code = "daikalop12a";
         this.isEnable = true;
-        this.historyDescription = "Núi Bà Đen là ngọn núi lửa đã tắt nằm ở trung tâm tỉnh Tây Ninh, Việt Nam. Với độ cao 986 m, đây là ngọn núi cao nhất miền Nam Việt Nam hiện nay, được mệnh danh \"Đệ nhất thiên sơn\". Trung tâm của quần thể tâm linh là điện Bà thờ Linh Sơn Thánh Mẫu, còn gọi là Bà Đen – vị mẫu thần có ơn phù hộ, độ trì, tương truyền có khả năng cảnh báo trước điềm dữ, tai ương.";
-        InitializeLocalization("Item1", "Item1_Description", "Item1_HistoryDescription");
     }
 
     public override void Activate()
